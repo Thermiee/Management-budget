b390e1fac0d072e50c647e44f135ae63868aa694
Thermiee <navyblack16@yahoo.com>
2022-07-07 22:57:32 +0100

    fix linters error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-  
-    has_many :groups
-    has_many :operations
-  
-    validates :name, presence: true, length: { in: 1..100 }
-  end
-  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :groups
+  has_many :operations
+
+  validates :name, presence: true, length: { in: 1..100 }
+end
